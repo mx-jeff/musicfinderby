187aaa9321a20ef1c669d3e329ade756f1ccabdc
Jeff <mx.jeferson.12@gmail.com>
2021-08-23 11:32:20 -0300

    deploy to heroku

--- src/controller.py
+++ src/controller.py
@@ -1,6 +1,5 @@
 from src.robots.youtube_scrapper_robot import scrapper_youtube_robot
 from src.robots.handle_video_youtube import download_and_convert_video_to_mp3
-from src.robots.music_file import Music
 
 
 def validade_input(crawler_name):
@@ -17,14 +16,12 @@ class Musicfinderby:
     crawler_name = "[Musicfinderby]"
     
     def __init__(self) -> None:
-        self.file_music = Music(self.crawler_name)
         print(f'{self.crawler_name} Iniciando crawler...')
 
     def search_url(self, name_music):
         return scrapper_youtube_robot(self.crawler_name, name_music)
     
     def download_and_convert(self, link):
-        self.file_music.remove()
         download_and_convert_video_to_mp3(self.crawler_name, link)
         return self.file_music.find()    
 
