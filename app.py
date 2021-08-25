0817e8dd6d0b4488e821f6dbb2810b52d958591f
Jeff <mx.jeferson.12@gmail.com>
2021-08-24 23:54:46 -0300

    deploy

--- app.py
+++ app.py
@@ -87,5 +87,13 @@ def getMusic():
         sleep(10)
         return redirect(url_for('getMusic'))
 
+
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
+
+
 if __name__ == "__main__":
     app.run()
