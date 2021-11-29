828f5ac2ed3e9f72ae309c1c657539c85c4a827e
Jeff <mx.jeferson.12@gmail.com>
2021-11-29 16:42:31 -0300

    update dependêncies

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ CORS(app, expose_headers=["Content-Disposition"]) #
 
 #config
 app.config['EXECUTOR_MAX_WORKERS'] = 1
+app.config['EXECUTOR_TYPE'] = 'thread'
 app.config['EXECUTOR_PROPAGATE_EXCEPTIONS'] = True
 NAME = "music"
 
